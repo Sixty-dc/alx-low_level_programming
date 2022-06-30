04079e8769605e19547d9b693dd7dc4c1c656f63
Sixty-dc <sikisi.mp@gmail.com>
2022-06-30 12:41:03 -0700

    edit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
-
-
 /**
  *
- *  * main - prints the phrase "Programming is like building a multilingual puzzle
+ * main - prints the phrase "Programming is like building a multilingual puzzle
  *
- *   * Return: 0 if exited properly, non-zero otherwise
+ * Return: 0 if exited properly, non-zero otherwise
  *
- *    */
+ */
 
 int main(void)
 
