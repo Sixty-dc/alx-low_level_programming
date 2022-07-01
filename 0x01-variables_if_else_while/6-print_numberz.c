e78a8669474cadacab433f7a1f0d5121118092cc
Sixty-dc <sikisi.mp@gmail.com>
2022-07-01 11:56:15 -0700

    fix print numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 /**
-* main - prints all single digit numbers of base 10 starting from 0,
-* followed by a new line, using putchar
-* Return: Always 0 (Success)
+* main - Prints the numbers since 0 to 9
+* Return: Always (Success)
+*
 */
 
 int main(void)
 {
 int n;
-for (n = 48; n < 58; n++)
+for (n = '0'; n <= '9'; n++)
 {
-putchar(n)
+putchar(n);
 }
 putchar('\n');
 return (0);
