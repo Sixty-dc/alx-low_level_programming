d1ec86b9ea8c9476922859d2293c9c763400b8ee
Sixty-dc <sikisi.mp@gmail.com>
2022-07-05 11:06:53 -0700

    files3

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,9 +2,9 @@
 
 /**
 * _isalpha - Check if character is a alphabet character.
+* @c: character to be checked.
 * Return: 1 if letter, lowercase or uppercase, and 0 otherwise
 */
-
 int _isalpha(int c)
 {
 if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
