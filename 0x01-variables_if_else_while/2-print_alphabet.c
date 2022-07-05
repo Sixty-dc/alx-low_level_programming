599f152e1dae0984dcb775c4909f65b8dd74e826
Sixty-dc <sikisi.mp@gmail.com>
2022-07-05 06:52:53 -0700

    fixed

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,8 +4,8 @@
 * main - prints the alphabet in lowercase,
 * followed by a new line
 * Return: Always 0 (Success)
-*/
-int main(void)
+ */
+int main(void)   
 {
 char ch;
 for (ch = 'a'; ch <= 'z'; ch++)
