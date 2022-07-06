c3c4aca9f9600e4cb73d19ff8a515b64cf0c2c4f
Sixty-dc <sikisi.mp@gmail.com>
2022-07-06 10:24:25 -0700

    r

--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -16,9 +16,12 @@ int year;
 month = 4;
 day = 01;
 year = 1997;
+
 printf("Date: %02d/%02d/%04d\n", month, day, year);
 
 day = convert_day(month, day);
+
 print_remaining_days(month, day, year);
+
 return (0);
 }
